27bb55bd1c3f6b539bbde7ce7e097fafc71872d3
heecheol.park <heecheol.park@linecorp.com>
2026-03-03 23:10:22 +0900

    feat: add confluence-cli formula

---
+++ Formula/confluence-cli.rb
@@ -0,0 +1,18 @@
+class ConfluenceCli < Formula
+  desc "Command-line interface for Atlassian Confluence"
+  homepage "https://github.com/pchuri/confluence-cli"
+  url "https://registry.npmjs.org/confluence-cli/-/confluence-cli-1.21.1.tgz"
+  sha256 "23c658262cd6a8d10eb44a002591d58f7ec40fd3f2b070ae703289e3d38aa3d6"
+  license "MIT"
+
+  depends_on "node"
+
+  def install
+    system "npm", "install", *std_npm_args
+    bin.install_symlink libexec.glob("bin/*")
+  end
+
+  test do
+    assert_match "confluence-cli", shell_output("#{bin}/confluence --help")
+  end
+end
---
+++ README.md
@@ -0,0 +1,21 @@
+# Homebrew Tap
+
+Homebrew formulae for [pchuri](https://github.com/pchuri) projects.
+
+## Available Formulae
+
+| Formula | Description |
+|---------|-------------|
+| confluence-cli | CLI for Atlassian Confluence |
+
+## Install
+
+```bash
+brew install pchuri/tap/confluence-cli
+```
+
+## Update
+
+```bash
+brew upgrade confluence-cli
+```
